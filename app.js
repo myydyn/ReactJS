4f93e3695f29f4464b64c0acf0b7ee4d78a2a700
Duyen <daphne.duyenhuynh@gmail.com>
2025-07-01 23:08:59 +0700

    5. Classes

--- app.js
+++ app.js
@@ -1,48 +1,4 @@
 
-// function logger(log) {
-//     console.log(log);
-// }
-// logger('Mmessage...');
-
-
-// expression function
-// const logger = function(log) {
-//     console.log(log);
-// }
-
-
-// 4. arrow function - hàm mũi tên
-// const logger = (log) => {
-//     console.log(log);
-// }
-// logger('Mmessage...');
-
-// const sum = (a, b) => a + b;
-// console.log(sum(2, 3));
-
-// const sum = (a, b) => ({a: a, b: b});
-// console.log(sum(2, 3));
-
-// const logger = log => console.log(log);
-// logger('i failed so much')
-
-// const course = {
-//     name: 'data bases',
-//     getName: function(){
-//         return this; //context
-//     }
-// };
-// console.log(course.getName());
-
-
-// const course =  function(name, price) { // arrow function k co constructor
-//     this.name = name;
-//     this.price = price;
-// }
-
-// const jscourse =  new course('data', 200);
-// console.log(jscourse);
-
 // 12. modules (split): import/export
 
 // import logger from './logger.js'
@@ -120,8 +76,8 @@
 
 // 2.template literals
 
-const courseName = 'java';
-const courseName2 = 'PHP';
+// const courseName = 'java';
+// const courseName2 = 'PHP';
 // const description = 'cource name: ' + courseName;
 // const description = `cource name: ${courseName} ${courseName2}`;
 // const description = `template string noi suy voi \${}`;
@@ -134,8 +90,96 @@ const courseName2 = 'PHP';
 //     + 'line 3\n'
 //     + 'line 4\n'  //tabnine: extention
 
-const lines = `line 1
-line 2
-line 33`
+// const lines = `line 1
+// line 2
+// line 33`
 
-console.log(lines);
+// console.log(lines);
+
+
+// function logger(log) {
+//     console.log(log);
+// }
+// logger('Mmessage...');
+
+
+// expression function
+// const logger = function(log) {
+//     console.log(log);
+// }
+
+
+// 4. arrow function - hàm mũi tên
+// const logger = (log) => {
+//     console.log(log);
+// }
+// logger('Mmessage...');
+
+// const sum = (a, b) => a + b;
+// console.log(sum(2, 3));
+
+// const sum = (a, b) => ({a: a, b: b});
+// console.log(sum(2, 3));
+
+// const logger = log => console.log(log);
+// logger('i failed so much')
+
+// const course = {
+//     name: 'data bases',
+//     getName: function(){
+//         return this; //context
+//     }
+// };
+// console.log(course.getName());
+
+
+// const course =  function(name, price) { // arrow function k co constructor
+//     this.name = name;
+//     this.price = price;
+// }
+
+// const jscourse =  new course('data', 200);
+// console.log(jscourse);
+
+
+// 5. classes
+// function course(Name, price){  //khuon mau: course
+//     this.Name = Name;
+//     this.price = price;
+
+//     this.getName = function(){
+//         return this.Name;
+//     }
+
+//     const isSuccess = false;
+// }
+// const phpCourse =  new course('php', 120);
+// const jsCourse =  new course('java', 120);
+// console.log(phpCourse);
+// console.log(jsCourse);
+
+    // class
+class course{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getPrice(){
+        return this.price;
+    }
+
+    run(){
+        const isSuccess = false;
+    }
+
+}
+const phpCourse =  new course('php', 120);
+const jsCourse =  new course('java', 1200);
+console.log(phpCourse);
+console.log(jsCourse);
+
