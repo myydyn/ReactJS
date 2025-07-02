5ec544b995b6cee3de429ab65788a99550621f7a
Duyen <daphne.duyenhuynh@gmail.com>
2025-07-02 14:41:43 +0700

    7. Default parameter values

--- app.js
+++ app.js
@@ -1,53 +1,4 @@
 
-// 12. modules (split): import/export
-
-// import logger from './logger.js'
-// console.log(logger)
-// logger('test message...', 'warn');
-
-// import logger from "./logger.js";
-// import {
-//     TYPE_LOG,
-//     TYPE_WARN,
-//     TYPE_ERROR
-// } from './constants.js';
-
-// logger('Test message...', TYPE_WARN);
-
-
-// import logger from "./logger.js";
-// import * as constants from './constants.js';
-
-// console.log(constants);
-
-
-// 6.  Enhanced object literals 
-// (định nghĩa key:value cho object)
-// (định nghĩa method cho object)
-// (định nghĩa key cho object dưới dạng biến)
-
-// var Courcename = 'data base';
-// var price = 100;
-
-// var course = {
-//     Courcename,
-//     price,
-//     getName() {
-//         return Courcename;
-//     }
-// };
-// console.log(course.getName());
-
-// var fieldName = 'name';
-// var fieldPrice = 'price';
-
-// const cource = {
-//     [fieldName]: 'java',
-//     [fieldPrice]: 100
-// };
-// console.log(cource);
-
-
 // 1. Let, const
 // phân biệt Var vs Let, Const: scope: phạm vi truy cập, hóting
 // const VS var, let: Assignment
@@ -159,27 +110,102 @@
 // console.log(jsCourse);
 
     // class
-class course{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-    }
+// class course{
+//     constructor(name, price){
+//         this.name = name;
+//         this.price = price;
+//     }
 
-    getName(){
-        return this.name;
-    }
+//     getName(){
+//         return this.name;
+//     }
 
-    getPrice(){
-        return this.price;
-    }
+//     getPrice(){
+//         return this.price;
+//     }
 
-    run(){
-        const isSuccess = false;
-    }
+//     run(){
+//         const isSuccess = false;
+//     }
 
-}
-const phpCourse =  new course('php', 120);
-const jsCourse =  new course('java', 1200);
-console.log(phpCourse);
-console.log(jsCourse);
+// }
+// const phpCourse =  new course('php', 120);
+// const jsCourse =  new course('java', 1200);
+// console.log(phpCourse);
+// console.log(jsCourse);
+
+
+
+// 6.  Enhanced object literals 
+// (định nghĩa key:value cho object)
+// (định nghĩa method cho object)
+// (định nghĩa key cho object dưới dạng biến)
+
+// var Courcename = 'data base';
+// var price = 100;
+
+// var course = {
+//     Courcename,
+//     price,
+//     getName() {
+//         return Courcename;
+//     }
+// };
+// console.log(course.getName());
+
+// var fieldName = 'name';
+// var fieldPrice = 'price';
+
+// const cource = {
+//     [fieldName]: 'java',
+//     [fieldPrice]: 100
+// };
+// console.log(cource);
+
+
+
+// 7. default parameter values
+// function logger(log = 'gia tri'){
+//     console.log(log);
+// }
+
+// logger()
+
+// function logger(log, type = 'log'){
+//     console[type](log);
+// }
+// logger("abc", 'warn');
+
+
+
+// 8. Destructuring
+
+
+
+// 9. rest parameters
+// 10. Spread
+// 11. tagged template literal
+
+
+// 12. modules (split): import/export
+
+// import logger from './logger.js'
+// console.log(logger)
+// logger('test message...', 'warn');
+
+// import logger from "./logger.js";
+// import {
+//     TYPE_LOG,
+//     TYPE_WARN,
+//     TYPE_ERROR
+// } from './constants.js';
+
+// logger('Test message...', TYPE_WARN);
+
+
+// import logger from "./logger.js";
+// import * as constants from './constants.js';
+
+// console.log(constants);
+
 
